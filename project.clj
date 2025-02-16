7d71c081b19913f503bcd6809534e70615ab4206
Onur Eker <onur@peerspace.com>
2025-02-15 20:07:08 -0800

    update clojars group and version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.yonureker/stripe-clojure "0.1.1"
+(defproject io.github.yonureker/stripe-clojure "0.1.2"
   :description "Clojure library for Stripe API"
   :url "http://github.com/yonureker/stripe-clojure"
   :license {:name "MIT License"
