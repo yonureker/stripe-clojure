04c5b5de4a3a2c286bdd400940559242529a3c57
Onur Eker <onur@peerspace.com>
2025-03-02 11:58:06 -0800

    update clients for test

--- test/stripe_clojure/test_util.clj
+++ test/stripe_clojure/test_util.clj
@@ -1,5 +1,10 @@
 (ns stripe-clojure.test-util
-  (:require [stripe-clojure.core :as stripe]))
+  (:require [stripe-clojure.core :as stripe]
+            [stripe-clojure.config :as config]))
 
-(def stripe-client (stripe/init-stripe {:mock true}))
+;; For mock tests, we use the stripe-mock server
+(def stripe-mock-client (stripe/init-stripe {:mock true}))
 
+;; For integration tests, we need to use a real Stripe API test key
+(def stripe-integration-client (stripe/init-stripe {:api-key (or (System/getenv "STRIPE_API_KEY") "test_key")
+                                                    :api-version config/base-api-version}))
