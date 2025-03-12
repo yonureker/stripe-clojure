b436efcd7b0adc2eba7e21c84cb5d277d9024457
Onur Eker <onur@peerspace.com>
2025-03-12 00:55:27 -0700

    add build.clj

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
 
 ;; Project configuration
 (def lib 'io.github.yonureker/stripe-clojure)
-(def version "0.1.4")
+(def version "0.2.0")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
